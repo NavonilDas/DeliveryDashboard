ed36181fe8514f01c74ed6a78d5c25d910b664aa
Navonil Das <navneeladas@gmail.com>
2020-11-21 14:30:02 +0530

    Click Listener for card and some styling

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,7 +5,7 @@ class Card extends React.Component {
     }
     render() {
         return (
-            <div className={`${this.props.active ? 'active' : ''} card`}>
+            <div className={`${this.props.active ? 'active' : ''} card`} onClick={this.props.onClick}>
                 <span>{this.props.name}</span>
                 <span style={{ marginLeft: "auto", fontSize: "2.5em" }}>{this.props.value}</span>
             </div>
