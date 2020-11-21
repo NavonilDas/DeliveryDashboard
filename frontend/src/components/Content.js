dea3229127ff83afdf4eb7f10da0b42635c87ed8
Navonil Das <navneeladas@gmail.com>
2020-11-21 14:52:26 +0530

    Function to Parse Date For Table

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -12,12 +12,21 @@ class Content extends React.Component {
             scan: []
         };
     }
-    
+
+    /**
+     * Function to parse DateTime String into format dd/mm/yyyy.
+     * @param {string} date DateTime
+     */
+    parseDate(date) {
+        let out = new Date(date);
+        let day = out.getDate();
+        let mon = out.getMonth();
+        return `${day < 10 ? '0' : ''}${day}/${mon < 10 ? '0' : ''}${mon}/${out.getFullYear()}`;
+    }
+
     render() {
         return (
             <div className="site-content">
-
-
             </div>
         );
     }
