741d70685fb207f2f3be452a08bc743b1416a006
Navonil Das <navneeladas@gmail.com>
2020-11-21 14:27:59 +0530

    Style Header Component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,19 +3,21 @@ class Header extends React.Component {
     render() {
         return (
             <header>
-                <img src="./imgs/logo.svg" alt="logo" />
+                <img src="./imgs/logo.svg" alt="logo" style={{ marginLeft: "50px" }} />
                 <h1>Intugine</h1>
                 <div style={{ flexGrow: 1 }}></div>
                 <ul>
-                    <li><a href="#">Home</a></li>
+                    <li className="active"><a href="#">Home</a></li>
                     <li><a href="#">Brands</a></li>
                     <li><a href="#">Transporters</a></li>
                 </ul>
-                <div className="profile-img">
-                    <img src="./imgs/profile.svg" />
-                </div>
-                <div className="down-arrow">
-                    <i className="fa fa-angle-down fa-2x" />
+                <div style={{ display: "flex" }} className="dropdown">
+                    <div className="profile-img">
+                        <img src="./imgs/profile.svg" />
+                    </div>
+                    <div className="down-arrow">
+                        <i className="fa fa-angle-down fa-2x" />
+                    </div>
                 </div>
             </header>
         );
